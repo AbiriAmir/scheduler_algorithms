4b09d091ef504a476b965acbb76a322e5af8a9d1
Amir <mr.abiri@gmail.com>
2017-01-19 17:09:43 +0330

    fix parameters

--- main.cpp
+++ main.cpp
@@ -10,19 +10,42 @@
 #define FIFO_ALGO "fifo"
 #define RANDOM_ALGO "random"
 
+#define DEFAULT_FILE_NAME "pinatrace.out"
+#define DEFAULT_NUMBER_OF_FRAMES "10%"
+#define DEFAULT_PAGE_SIZE "4k"
+
 using namespace std;
 
 typedef long long ll;
+
 int main(int argc, char *argv[]) {
 
 	if(argc < 9) {
 		return -1;
 	}
 
-	string algorithm_str(argv[2]);
-	string file_name(argv[4]);
-	string num_of_frames(argv[6]);
-	string page_size_str(argv[8]);
+	string algorithm_str = FIFO_ALGO; // (argv[2]);
+	string file_name = DEFAULT_FILE_NAME; // (argv[4]);
+	string num_of_frames = DEFAULT_NUMBER_OF_FRAMES; // (argv[6]);
+	string page_size_str = DEFAULT_PAGE_SIZE; //(argv[8]);
+
+	for(int i = 0; i < argc - 1; ++i) {
+		string 
+			tmp(argv[i]),
+			tmp2(argv[i+1]);
+
+		if(tmp == "--algorithm")
+			algorithm_str = tmp2;
+
+		if(tmp == "--trace")
+			file_name = tmp2;
+
+		if(tmp == "--pages")
+			num_of_frames = tmp2;
+
+		if(tmp == "--page_size")
+			page_size_str = tmp2;
+	}
 
 
 	vector< pair<long long, int> > references;
